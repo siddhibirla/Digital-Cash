012d74f2cbb7ba757da92050255a5f2967a68f8e
Saurabh Shirur <33301817+saushirur1@users.noreply.github.com>
2018-04-27 00:42:00 -0700

    Add files via upload
    Merchant class created

---
+++ Merchant.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.math.BigInteger;
+public class Merchant
+{
+public Merchant()
+{
+  System.out.println("Merchant class created");
+}
+}
